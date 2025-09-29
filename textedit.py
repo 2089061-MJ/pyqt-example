47707f4d820edebb1584f4c80f8d76855ddd5b60
2089061-MJ <2089061@pcu.ac.kr>
2025-09-29 15:35:52 +0900

    Add files via upload
    Qvbox 레이아웃 예제 추가

---
+++ textedit.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtCore import Qt
+
+form_class = uic.loadUiType("textedit.ui")[0]
+
+class Window(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        
+        self.plainTextEdit.setPlainText("여기에 글을 입력해보세요! \n\n=>" )
+        self.btnClear.clicked.connect(self.clearText)
+        
+    def clearText(self):
+        self.plainTextEdit.clear()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = Window()
+    myWindow.show()
+    app.exec_()
